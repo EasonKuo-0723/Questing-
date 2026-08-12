78a8337706341185189126fe77c32136d8e07b11
Eason <easonkuo980723@gmail.com>
2026-08-12 16:32:20 -0700

    Add Xcode project setup

---
+++ Questing/QuestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestingApp.swift
+//  Questing
+//
+//  Created by Eason Kuo on 8/10/26.
+//
+
+import SwiftUI
+
+@main
+struct QuestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
